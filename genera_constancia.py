fb350894d3f8bd2d40403f4a9b4579398526bce5
puic2024 <em0423572@gmail.com>
2024-07-03 20:35:02 -0600

    Add files via upload

--- genera_constancia.py
+++ genera_constancia.py
@@ -1,15 +1,25 @@
 import streamlit as st
 import pandas as pd
 # Título de la aplicación
-st.title("Cargar y Mostrar Archivo de Excel")
-# Subir el archivo de Excel
-uploaded_file = st.file_uploader("Elige un archivo de Excel", type=["xlsx"])
+st.title("Cargar y Mostrar Archivo CSV")
+# Descripción de la aplicación
+st.write("""
+Esta aplicación te permite cargar un archivo CSV y visualizar su contenido en forma de DataFrame.
+""")
+# Subir el archivo CSV
+uploaded_file = st.file_uploader("Elige un archivo CSV", type=["csv"])
 # Si se sube un archivo
 if uploaded_file is not None:
-   # Leer el archivo de Excel
-   df = pd.read_excel(uploaded_file)
-   # Mostrar el DataFrame
-   st.write("DataFrame del archivo de Excel:")
-   st.dataframe(df)
+   try:
+       # Leer el archivo CSV
+       df = pd.read_csv(uploaded_file)
+       # Mostrar algunas estadísticas básicas del DataFrame
+       st.write("## Estadísticas del DataFrame:")
+       st.write(df.describe())
+       # Mostrar el DataFrame
+       st.write("## Contenido del DataFrame:")
+       st.dataframe(df)
+   except Exception as e:
+       st.error(f"Error al leer el archivo CSV: {e}")
 else:
-   st.write("Por favor, sube un archivo de Excel para continuar.")
+   st.info("Por favor, sube un archivo CSV para continuar.")
