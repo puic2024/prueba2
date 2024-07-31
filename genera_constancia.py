8c82c34b55d39d4d7d049d2396940631101e893d
puic2024 <em0423572@gmail.com>
2024-07-30 21:02:30 -0600

    Update genera_constancia.py

--- genera_constancia.py
+++ genera_constancia.py
@@ -34,30 +34,34 @@ uploaded_file = st.file_uploader("Cargar CSV", type=["csv"])
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, encoding='utf-8')
+    df.fillna('', inplace=True)  # Rellenar valores nulos con una cadena vacía
     st.write("DataFrame:")
     st.dataframe(df)
 
-    if st.button("Generar PDFs"):
-        pdf_files = []
-        for index, row in df.iterrows():
-            data = row.to_dict()
-            pdf_filename = f"{data['nombre']}.pdf"  # Ajusta según el campo de nombre
-            generate_pdf(data, pdf_filename)
-            pdf_files.append(pdf_filename)
-        
-        zip_filename = "pdf_files.zip"
-        create_zip(pdf_files, zip_filename)
-        
-        with open(zip_filename, "rb") as f:
-            bytes_data = f.read()
-            st.download_button(
-                label="Descargar ZIP",
-                data=bytes_data,
-                file_name=zip_filename,
-                mime="application/zip"
-            )
-        
-        # Limpiar archivos temporales
-        for pdf_file in pdf_files:
-            os.remove(pdf_file)
-        os.remove(zip_filename)
+    if 'nombre' not in df.columns:
+        st.error("El archivo CSV debe contener una columna 'nombre'.")
+    else:
+        if st.button("Generar PDFs"):
+            pdf_files = []
+            for index, row in df.iterrows():
+                data = row.to_dict()
+                pdf_filename = f"{data['nombre']}.pdf"
+                generate_pdf(data, pdf_filename)
+                pdf_files.append(pdf_filename)
+            
+            zip_filename = "pdf_files.zip"
+            create_zip(pdf_files, zip_filename)
+            
+            with open(zip_filename, "rb") as f:
+                bytes_data = f.read()
+                st.download_button(
+                    label="Descargar ZIP",
+                    data=bytes_data,
+                    file_name=zip_filename,
+                    mime="application/zip"
+                )
+            
+            # Limpiar archivos temporales
+            for pdf_file in pdf_files:
+                os.remove(pdf_file)
+            os.remove(zip_filename)
