0776a2a3fc73796d811a04db93d4e284f1737ce1
puic2024 <em0423572@gmail.com>
2024-08-14 14:51:30 -0600

    Update genera_constancia.py

--- genera_constancia.py
+++ genera_constancia.py
@@ -1,4 +1,8 @@
+import streamlit as st
+import pandas as pd
 from fpdf import FPDF
+import zipfile
+import os
 from PIL import Image
 
 # Función para generar el PDF con una imagen de fondo y texto centrado
@@ -27,3 +31,55 @@ def generate_pdf(data, filename, background_image):
         y_start += line_height  # Mover la posición y para la siguiente línea de texto
     
     pdf.output(filename)
+
+# Función para crear archivos ZIP
+def create_zip(pdf_files, zip_filename):
+    with zipfile.ZipFile(zip_filename, 'w') as zipf:
+        for pdf_file in pdf_files:
+            zipf.write(pdf_file, os.path.basename(pdf_file))
+
+# Configuración de Streamlit
+st.title("Generador de constancias PUIC")
+
+# Mostrar imagen al principio
+st.image("imagenes/escudo.jpg")
+
+# Cargar archivo CSV
+uploaded_file = st.file_uploader("Cargar CSV", type=["csv"])
+background_image = st.file_uploader("Cargar imagen de fondo", type=["png"])
+
+if uploaded_file is not None and background_image is not None:
+    df = pd.read_csv(uploaded_file, encoding='utf-8')
+    st.write("DataFrame:")
+    st.dataframe(df)
+
+    if st.button("Generar PDFs"):
+        # Guardar la imagen de fondo en un archivo temporal
+        bg_image_path = "background_image.png"
+        with open(bg_image_path, "wb") as f:
+            f.write(background_image.read())
+        
+        pdf_files = []
+        for index, row in df.iterrows():
+            data = row.to_dict()
+            pdf_filename = f"{data['nombre']}.pdf"  # Ajusta según el campo de nombre
+            generate_pdf(data, pdf_filename, bg_image_path)
+            pdf_files.append(pdf_filename)
+        
+        zip_filename = "pdf_files.zip"
+        create_zip(pdf_files, zip_filename)
+        
+        with open(zip_filename, "rb") as f:
+            bytes_data = f.read()
+            st.download_button(
+                label="Descargar ZIP",
+                data=bytes_data,
+                file_name=zip_filename,
+                mime="application/zip"
+            )
+        
+        # Limpiar archivos temporales
+        for pdf_file in pdf_files:
+            os.remove(pdf_file)
+        os.remove(zip_filename)
+        os.remove(bg_image_path)
