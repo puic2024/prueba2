8417eb972403b284a3782a861dac6664b3b7e2e6
puic2024 <em0423572@gmail.com>
2024-08-14 16:43:59 -0600

    Update genera_constancia.py

--- genera_constancia.py
+++ genera_constancia.py
@@ -17,7 +17,7 @@ def generate_pdf(data, filename, background_image):
     pdf.set_font("Arial", size=24)  # Reducir tamaño de la fuente a la mitad
     
     # Variables para centrar el texto verticalmente y luego bajarlo 30 píxeles
-    line_height = pdf.font_size * 2  # Altura de cada línea de texto
+    line_height = pdf.font_size * 1  # Altura de cada línea de texto
     total_text_height = line_height * len(data)  # Altura total del bloque de texto
     
     #y_start = (1275 - total_text_height) / 2 + 70  # Posición inicial en el eje y para centrar y bajar el texto
@@ -26,7 +26,7 @@ def generate_pdf(data, filename, background_image):
     # Ajustar el texto sobre el fondo, centrado horizontal y verticalmente
     for value in data.values():
         text = str(value)  # Convertir el valor a cadena de texto
-        text_width = pdf.get_string_width(text) + 1
+        text_width = pdf.get_string_width(text) + 6
         pdf.set_xy((1650 - text_width) / 2, y_start)
         pdf.cell(text_width, line_height, text, ln=True, align='C')
         y_start += line_height  # Mover la posición y para la siguiente línea de texto
