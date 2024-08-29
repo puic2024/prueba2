fe1818d9b3aaf956236161a19fcecb6db584e543
puic2024 <em0423572@gmail.com>
2024-08-28 20:28:39 -0600

    Update genera_constancia.py

--- genera_constancia.py
+++ genera_constancia.py
@@ -3,10 +3,10 @@ import pandas as pd
 from fpdf import FPDF
 import zipfile
 import os
-from PIL import Image
+import ast
 
-# Función para generar el PDF con una imagen de fondo y texto centrado
-def generate_pdf(data, filename, background_image):
+# Función para generar el PDF con una imagen de fondo y texto parametrizado
+def generate_pdf(data, filename, background_image, font_settings):
     # Crear un objeto FPDF con dimensiones personalizadas
     pdf = FPDF(unit='pt', format=[1650, 1275])
     pdf.add_page()
@@ -14,22 +14,21 @@ def generate_pdf(data, filename, background_image):
     # Cargar la imagen de fondo y ajustar el tamaño al tamaño de la página
     pdf.image(background_image, x=0, y=0, w=1650, h=1275)
     
-    pdf.set_font("Arial", size=30)  # Reducir tamaño de la fuente a la mitad
-    
     # Variables para centrar el texto verticalmente y luego bajarlo 30 píxeles
-    line_height = pdf.font_size * 1.5  # Altura de cada línea de texto
-    total_text_height = line_height * len(data)  # Altura total del bloque de texto
-    
-    #y_start = (1275 - total_text_height) / 2 + 70  # Posición inicial en el eje y para centrar y bajar el texto
     y_start = 470
     
     # Ajustar el texto sobre el fondo, centrado horizontal y verticalmente
-    for value in data.values():
-        text = str(value)  # Convertir el valor a cadena de texto
-        text_width = pdf.get_string_width(text) + 6
-        pdf.set_xy((1650 - text_width) / 2, y_start)
-        pdf.cell(text_width, line_height, text, ln=True, align='C')
-        y_start += line_height  # Mover la posición y para la siguiente línea de texto
+    for key, value in data.items():
+        if key in font_settings:
+            text = str(value)  # Convertir el valor a cadena de texto
+            font_size = font_settings[key]['tamaño']
+            font_type = font_settings[key]['tipo_letra']
+            pdf.set_font(font_type, size=font_size)
+            text_width = pdf.get_string_width(text) + 6
+            line_height = pdf.font_size * 1.5
+            pdf.set_xy((1650 - text_width) / 2, y_start)
+            pdf.cell(text_width, line_height, text, ln=True, align='C')
+            y_start += line_height  # Mover la posición y para la siguiente línea de texto
     
     pdf.output(filename)
 
@@ -48,12 +47,16 @@ st.image("imagenes/escudo.jpg")
 # Cargar archivo CSV
 uploaded_file = st.file_uploader("Cargar CSV", type=["csv"])
 background_image = st.file_uploader("Cargar imagen de fondo", type=["png"])
+font_file = st.file_uploader("Cargar archivo de configuración de fuentes (txt)", type=["txt"])
 
-if uploaded_file is not None and background_image is not None:
+if uploaded_file is not None and background_image is not None and font_file is not None:
     df = pd.read_csv(uploaded_file, encoding='utf-8')
     st.write("DataFrame:")
     st.dataframe(df)
 
+    # Leer el archivo de configuración de fuentes
+    font_settings = ast.literal_eval(font_file.read().decode('utf-8'))
+    
     if st.button("Generar PDFs"):
         # Guardar la imagen de fondo en un archivo temporal
         bg_image_path = "background_image.png"
@@ -64,7 +67,7 @@ if uploaded_file is not None and background_image is not None:
         for index, row in df.iterrows():
             data = row.to_dict()
             pdf_filename = f"{data['nombre']}.pdf"  # Ajusta según el campo de nombre
-            generate_pdf(data, pdf_filename, bg_image_path)
+            generate_pdf(data, pdf_filename, bg_image_path, font_settings)
             pdf_files.append(pdf_filename)
         
         zip_filename = "pdf_files.zip"
